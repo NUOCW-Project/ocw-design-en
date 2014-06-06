63664ca99c091894c9f305ff8ace60999233ed5b
kojima <kojima@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2014-06-06 07:26:39 +0000

    js edit for voice
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/3.0@8212 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/voice.js
+++ js/voice.js
@@ -1,8 +1,19 @@
 $(function(){
+
 // back to top
-    $(".back").click(function () {
-    $('html,body').animate({ scrollTop: 0 }, 'fast');
-    return false;
-    });
+    
+//    $(".back").click(function () {
+//    $('html,body').animate({ scrollTop: 0 }, 'fast');
+//    return false;
+//    });
+
+//});
+
+     $('.back').click(function (){
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+     });
 
 });
