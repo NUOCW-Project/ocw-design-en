addc59116e7606e505627047b6ea1c944fddabf3
toida-y <toida-y@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2014-06-09 10:40:46 +0000

    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/3.0@8245 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/course.js
+++ js/course.js
@@ -88,7 +88,7 @@ $(function(){
 // back to top
     $('.close_section').click(function (event) {
     	// event : event object
-    	$(this).parent().click();
+    	$('#' + $(this).parent().attr('id').split('_')[0]).click();
 /*        var i = $("#class_info").index(this)
         var p = $("#class_info").eq(i).offset().top;
         $('html,body').animate({ scrollTop: p }, 'fast');
