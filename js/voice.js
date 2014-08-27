79e5493f52179f4fe005d7e57287f13098ec4b9c
kojima <kojima@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2014-08-27 06:01:47 +0000

    tweetボタンにjQueryでcss 「top: 15px;」 を指定;iframeのためcssがきかない
    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/3.0@8799 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/voice.js
+++ js/voice.js
@@ -6,8 +6,10 @@ $(document).ready(function(){
             var p = $("#browse_jump").eq(i).offset().top;
         $('html,body').animate({ scrollTop: p }, 'fast');
             return false;
-
+    
 });
+
+        $("#twitter-widget-0").css("top","15px");
    
 });
 
