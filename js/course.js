50686a8f8c417589701862d974ef02821f4abfbc
toida-y <toida-y@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2014-10-01 09:25:54 +0000

    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/3.0@8834 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/course.js
+++ js/course.js
@@ -1,115 +1,54 @@
-$(function(){
+/// <reference path="//code.jquery.com/jquery-2.1.0.js">
+
+$(function () {
 
 	// class_detail display
 
 	var hash = new String(location.hash);
 
-    var box_id_list = new Array(
-        new Array('#overview', '#overview_contents'), 
-        new Array('#syllabus', '#syllabus_contents'), 
-//      new Array(), 
-        new Array('#materials', '#materials_contents')
+	var box_id_list = new Array(
+        'overview', 'syllabus', 'materials'
     );
     
-    $('hr').css("display", "none");
+    $('#main_contents > hr').css('display', 'none');
 
-    $(box_id_list[0][1]).css("display", "none");
-    $('.open', box_id_list[0][0]).css("display", "none");
-    $(box_id_list[0][0]).click(function () {
-    	var flag = true;
+    for (var i = 0; i < box_id_list.length; i++)
+    {
+        // ループ変数がコールバック関数で正常に展開されないことに対する対策
+        new function () {
+            var button = $('#' + box_id_list[i]);
+            var box = $('#' + box_id_list[i] + '_contents')
 
-    	$(box_id_list[0][1]).toggle();
-    	$('img', box_id_list[0][0]).toggle();
+            box.css('display', 'none');
+            $('.open', '#' + box_id_list[i]).css('display', 'none');
 
-    	for (var j = 1; j < box_id_list.length; j++)
-    		flag = flag && ($(box_id_list[j][0]).length == 0);
-    	if (flag)
-    		$('hr').toggle(); // Last update line
-    });
+            // コールバック関数登録
+            button.click(function () {
+                button.children('h2').children('img').toggle();
+                box.slideToggle(1000);
 
-    $(box_id_list[1][1]).css("display", "none");
-    $('.open', box_id_list[1][0]).css("display", "none");
-    $(box_id_list[1][0]).click(function () {
-    	var flag = true;
+                // 末尾項目クリックの検出
+                var flag = true;
+                for (var j = i; j < box_id_list.length; j++)
+                    // 全ての項目が存在するとは限らないので, 項目の存在を走査
+                    flag = flag && ($('#' + box_id_list[j]).length == 0);
+                if (flag)
+                    $('#main_contents > hr').toggle(); // Last update line
+            });
 
-    	$(box_id_list[1][1]).toggle();
-    	$('img', box_id_list[1][0]).toggle();
-
-    	for (var j = 2; j < box_id_list.length; j++)
-    		flag = flag && ($(box_id_list[j][0]).length == 0);
-    	if (flag)
-    		$('hr').toggle(); // Last update line
-    });
-
-    $(box_id_list[2][1]).css("display", "none");
-    $('.open', box_id_list[2][0]).css("display", "none");
-    $(box_id_list[2][0]).click(function () {
-    	var flag = true;
-
-    	$(box_id_list[2][1]).toggle();
-    	$('img', box_id_list[2][0]).toggle();
-
-    	for (var j = 3; j < box_id_list.length; j++)
-    		flag = flag && ($(box_id_list[j][0]).length == 0);
-    	if (flag)
-    		$('hr').toggle(); // Last update line
-    });
-
-
-    if (hash != '') {
-    	$(location.hash).click();
- //   	location.hash = hash;
+            $('.close_section', '#' + box_id_list[i] + '_contents').click(function () {
+                $('html, body').animate({ scrollTop: button.offset().top }, 500);
+                button.click();
+            });
+        }
     }
 
-/*
-    $("#box2").css("display", "none"); 
-    $("#open2").css("display", "none");
-    $("#push2").click(function(){
-        $("#box2").toggle();
-        $(".close_open2").toggle();
-    });
-    
-    $("#box3").css("display", "none"); 
-    $("#open3").css("display", "none");
-    $("#push3").click(function(){
-        $("#box3").toggle();
-        $(".close_open3").toggle();
-    });
-    
-    $("#box4").css("display", "none"); 
-    $("#open4").css("display", "none");
-    $("hr").css("display", "none");
-    $("#push4").click(function(){
-        $("#box4").toggle();
-        $(".close_open4").toggle();
-        $("hr").toggle(); // Last update line
-    });	
-	*/
-// back to top
-    $('.close_section').click(function (event) {
-    	// event : event object
-    	var hash = '#' + $(this).parent().attr('id').split('_')[0];
-    	$(hash).click();
-    	location.hash = hash;
-/*        var i = $("#class_info").index(this)
-        var p = $("#class_info").eq(i).offset().top;
-        $('html,body').animate({ scrollTop: p }, 'fast');
- */   });
-
-//    $("#category_name").click(function () {
-//        var i = $("#category_name").index(this)
-//        var p = $("#category").eq(i).offset().top;
-//        $('html,body').animate({ scrollTop: p }, 'fast');
-//        return false;
-//    });
-
     $(window).load(function () {
-    	var hash = location.hash;
-    	if (hash != '') {
-    		//    		jQuery('html,body').animate({ scrollTop: $(location.hash).offset.top }, 'fast');
-    		location.hash = '';
-    		location.hash = hash;
-    	}
+        var hash = location.hash;
+
+    	if (hash != '')
+    	    $(hash).click();
+    	else
+    	    $('#' + box_id_list[0]).click();
     });
-    $(box_id_list[0][0]).click();
 });
