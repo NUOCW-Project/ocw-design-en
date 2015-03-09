f2cd38f3c427648c750af95c8fb60906fd596fb8
takaichi-d <takaichi-d@0cb0e2da-76ef-49b7-9531-4241df5af7e9>
2015-03-09 17:50:58 +0000

    git-svn-id: file:///ocw/svn/design-rep/ocw-design/branches/3.0u@9003 0cb0e2da-76ef-49b7-9531-4241df5af7e9

--- js/noDisplay.js
+++ js/noDisplay.js
@@ -91,10 +91,8 @@ $(document).ready(function(){
         $("nav").eq(index).removeClass("bg_off");
         $("nav").eq(index).addClass("bg_on");
 
-        $("nav a img").each(function () {
-            $(this).attr("src", $("nav a img").attr("src").replace("_on", "_lead"));
-        });
-        $("nav a img").eq(index).attr("src", $("nav a img").attr("src").replace("_lead", "_on"));
+        $("nav a img").attr("src", $("nav a img").attr("src").replace("_on", "_lead"));
+        $("nav a img").eq(index).attr("src", $("nav a img").eq(index).attr("src").replace("_lead", "_on"));
     });
 
 });
