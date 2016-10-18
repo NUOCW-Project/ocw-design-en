f5c6e6ef82725f1d03394a4369ab294790e21842
takano <takano.yukina@c.mbox.nagoya-u.ac.jp>
2016-10-18 13:35:54 +0900

    CSS,JSファイル編集 refs #1299

--- js/lab_intro.js
+++ js/lab_intro.js
@@ -4,16 +4,23 @@ $(function() {
     'lite', 'educ', 'law', 'econ', 'info', 'scie', 'medi', 'engi', 'agri', 'other'
   );
 
+  //はじめに学部ごとのはすべて非表示にする
+  for(var i = 0; i < depart_id_list.length; i++){
+    $('.movie.' + depart_id_list[i]).css('display', 'none');
+  }
+
   //学部を選択したとき
-  $('.department_list img').click(function() {
+  $('#department_list li').click(function() {
     //クリックされたリスト番号取得
-    var index = $('.department_list img').index(this);
+    var index = $('#department_list li').index(this);
     var img_dir = "./images/ja/lab_introduction/button/"+depart_id_list[index]+".svg";
+
+    console.log(index);
 
     //動画が存在する学部をクリックした場合のみ動作
     if(index != 3 || index != 6 || index != 9){
       //一度タブについているクラスselectedを消す
-      $('#department_list img').removeClass('selected');
+      $('#department_list li').removeClass('selected');
       //クリックされたタブのみにクラスselectedをつける
       $(this).addClass('selected');
       //クリックした画像を変更
@@ -26,6 +33,7 @@ $(function() {
       }
       //クリックされた学部のコンテンツを表示
       $('.movie.' + depart_id_list[index]).css('display','block');
+      console.log(depart_id_list[index]);
     }
   });
 });
