47c8911c8b02926f7bd6b8d123b355bfc686d1ae
Koshi Oi <koushi-0537@ezweb.ne.jp>
2018-07-12 20:11:23 +0900

    #1898-specialtopics-rimeiku

--- js/specialtopics.js
+++ js/specialtopics.js
@@ -1,9 +1,9 @@
 $(function(){
     $("#paging").pagination({
-        items: 8, //ページングの数
+        items: 2, //ページングの数
         displayedPages: 1, //表示したいページング要素数
-        prevText: '前', //前へのリンクテキスト
-        nextText: '次', //次へのリンクテキスト
+        prevText: '<', //前へのリンクテキスト
+        nextText: '>', //次へのリンクテキスト
         cssStyle: 'light-theme' //テーマ"dark-theme"、"compact-theme"があります
     })
 });
